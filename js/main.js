1e6aa35178e8a80e2f38b0a986ac9543e3038350
nelyflores <nely.afloresr@gmail.com>
2017-06-04 15:55:20 -0500

    cambiando de javascript a jquery

--- js/main.js
+++ js/main.js
@@ -1,60 +1,50 @@
 
-var btnagregar=document.getElementById('agregar');
-btnagregar.addEventListener("click",generar);
+function cargarPagina(){
+  $("#agregar").click(generar);
+};
+
 
 function generar(){
-  var total=document.getElementById("contactostotal");
-  var numerocontactos=document.getElementsByClassName('collection-item');
-  console.log(numerocontactos);
-  var totalcontacto=numerocontactos.length;
-  var nuevonombre=document.getElementById('nombre').value;
-  var nuevotelefono=document.getElementById("telefono").value;
-  var nuevomail=document.getElementById("email").value;
-  var lista= document.getElementById('listas');
-  var elementolista= document.createElement("li");
-  elementolista.classList.add("collection-item","avatar");
-  lista.appendChild(elementolista);
-
-  var imagen= document.createElement("img");
-  imagen.classList.add("circle");
-  imagen.setAttribute('src','img/persona.jpg');
-  elementolista.appendChild(imagen);
-
-  var titulo=document.createElement("span");
-  titulo.classList.add("title");
-  elementolista.appendChild(titulo);
-  titulo.innerHTML=nuevonombre;
-
-  var parrafoTelefono=document.createElement("p");
-  elementolista.appendChild(parrafoTelefono);
-
-
-  var iconotelefono=document.createElement("i");
-  iconotelefono.classList.add("tiny", "material-icons");
-  iconotelefono.innerHTML="phone";
-  parrafoTelefono.appendChild(iconotelefono);
-  var telefono=document.createTextNode(nuevotelefono);
-  parrafoTelefono.appendChild(telefono);
-
-  var parrafomail=document.createElement("p");
-  elementolista.appendChild(parrafomail);
-
-  var iconomail=document.createElement("i");
-  iconomail.classList.add("tiny", "material-icons");
-  iconomail.innerHTML="mail";
-  parrafomail.appendChild(iconomail);
-  var mail=document.createTextNode(nuevomail);
-  parrafomail.appendChild(mail);
-
-
-  var estrella=document.createElement("a");
-  estrella.classList.add("secondary-content");
-  elementolista.appendChild(estrella);
-
-  var iconoestrella=document.createElement("i");
-  iconoestrella.classList.add( "material-icons");
-  iconoestrella.innerHTML="grade";
-  estrella.appendChild(iconoestrella);
-  total.innerHTML=totalcontacto;
+  //obtenemos todos los valores
+  var $total=$("#contactostotal");
+  var $numerocontactos= $(".collection-item");
+  var $nuevonombre=$("#nombre").val();
+  var $nuevotelefono=$("#telefono").val();
+  var $nuevomail=$("#email").val();
+  var $lista=$("#listas");
+  //creamos elementos
+  var $elementolista=$("<li>",{'class':'collection-item avatar'});
+  var $imagen=$("<img>",{"class":"circle"});
+  var $titulo=$("<span>",{"class":"title"});
+  var $nombre=document.createTextNode($nuevonombre);
+  var $parrafoTelefono=$("<p>");
+  var $iconotelefono=$("<i>",{"class":"tiny material-icons"});
+  var $telefono=document.createTextNode($nuevotelefono);
+  var $parrafomail=$("<p>");
+  var $iconomail=$("<i>",{"class":"tiny, material-icons"});
+  var $mail=document.createTextNode($nuevomail);
+  var $estrella=$("<a>",{"class":"secondary-content"});
+  var $iconoestrella=$("<i>",{"class":"material-icons"});
+  //Agrgamos los nodos e imprimos en el HTML
+  $lista.append($elementolista);
+  $imagen.attr('src','img/persona.jpg');
+  $elementolista.append($imagen);
+  $elementolista.append($titulo);
+  $titulo.append($nombre);
+  $elementolista.append($parrafoTelefono);
+  $iconotelefono.text="phone";
+  $parrafoTelefono.append($iconotelefono);
+  $parrafoTelefono.append($telefono);
+  $elementolista.append($parrafomail);
+  $iconomail.text="mail";
+  $parrafomail.append($iconomail);
+  $parrafomail.append($mail);
+  $elementolista.append($estrella);
+  $iconoestrella.text="grade";
+  $estrella.append($iconoestrella);
+  var $totalcontacto=$numerocontactos.length;
+  console.log($totalcontacto);
+  $total.text=$totalcontacto;
 
 }
+	$(document).ready(cargarPagina);
